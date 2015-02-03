cd71a0134b56fd18552b983f85de2ef2f6a3ab08
cnGoku <406342509@qq.com>
2015-02-03 18:04:50 +0800

    Edge Coloring

---
+++ project2/project2/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.导入ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.readmeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nv = new System.Windows.Forms.TextBox();
+            this.mv = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(74, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // input
+            // 
+            this.input.BackColor = System.Drawing.Color.White;
+            this.input.Location = new System.Drawing.Point(37, 116);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.input.Size = new System.Drawing.Size(246, 227);
+            this.input.TabIndex = 1;
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.White;
+            this.output.Location = new System.Drawing.Point(327, 89);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(269, 254);
+            this.output.TabIndex = 2;
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.White;
+            this.panel.Location = new System.Drawing.Point(630, 89);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(300, 300);
+            this.panel.TabIndex = 3;
+            this.panel.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(96, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(407, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(738, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Image";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(960, 25);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.导入ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.打开ToolStripMenuItem.Text = "File";
+            // 
+            // 导入ToolStripMenuItem
+            // 
+            this.导入ToolStripMenuItem.Name = "导入ToolStripMenuItem";
+            this.导入ToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.导入ToolStripMenuItem.Text = "Import";
+            this.导入ToolStripMenuItem.Click += new System.EventHandler(this.导入ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.退出ToolStripMenuItem.Text = "Exit";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem,
+            this.readmeToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.帮助ToolStripMenuItem.Text = "Help";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.关于ToolStripMenuItem.Text = "About";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // readmeToolStripMenuItem
+            // 
+            this.readmeToolStripMenuItem.Name = "readmeToolStripMenuItem";
+            this.readmeToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.readmeToolStripMenuItem.Text = "Readme";
+            this.readmeToolStripMenuItem.Click += new System.EventHandler(this.readmeToolStripMenuItem_Click);
+            // 
+            // nv
+            // 
+            this.nv.BackColor = System.Drawing.Color.White;
+            this.nv.Location = new System.Drawing.Point(184, 89);
+            this.nv.Name = "nv";
+            this.nv.Size = new System.Drawing.Size(62, 21);
+            this.nv.TabIndex = 8;
+            // 
+            // mv
+            // 
+            this.mv.BackColor = System.Drawing.Color.White;
+            this.mv.Location = new System.Drawing.Point(78, 89);
+            this.mv.Name = "mv";
+            this.mv.Size = new System.Drawing.Size(61, 21);
+            this.mv.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(51, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 14);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "m:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(157, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 14);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "n:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Title = "Import";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 420);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mv);
+            this.Controls.Add(this.nv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "李玉翎飞-project2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 导入ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.TextBox nv;
+        private System.Windows.Forms.TextBox mv;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem readmeToolStripMenuItem;
+    }
+}
+
---
+++ project2/project2/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace project2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel.CreateGraphics();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (draw)
+            {
+                //Draw
+                g.Clear(Color.White);
+                //Calculate the posotion of each vertex
+                double angle = Math.PI * 2 / m;
+                int[,] mPosition = new int[m, 2];
+                for (int i = 0; i != m; ++i)
+                {
+                    mPosition[i, 0] = 150 + Convert.ToInt32(Math.Cos(i * angle) * 120);//x-coordinate
+                    mPosition[i, 1] = 150 + Convert.ToInt32(Math.Sin(i * angle) * 120);//y-coordinate
+                }
+                for (int j = 0; j != n; ++j)
+                {
+                    Pen pen = null;
+                    switch (Ecolor[j])
+                    {
+                        case 1: pen = new Pen(Color.Red, 2); break;
+                        case 2: pen = new Pen(Color.Orange, 2); break;
+                        case 3: pen = new Pen(Color.Yellow, 2); break;
+                        case 4: pen = new Pen(Color.Green, 2); break;
+                        case 5: pen = new Pen(Color.Blue, 2); break;
+                        case 6: pen = new Pen(Color.Indigo, 2); break;
+                        case 7: pen = new Pen(Color.Violet, 2); break;
+                    }
+                    int u = -1, v = 0;
+                    for (int i = 0; i != m; ++i)
+                        if (IM[i, j] == 1)
+                            if (u == -1)
+                                u = i;
+                            else
+                            {
+                                v = i;
+                                break;
+                            }
+                    Point p1 = new Point(mPosition[u, 0], mPosition[u, 1]);
+                    Point p2 = new Point(mPosition[v, 0], mPosition[v, 1]);
+                    g.DrawLine(pen, p1, p2);
+                }
+            }
+        }
+
+        private int[,] IM = null;
+        private int m = 0;
+        private int n = 0;
+        private int[] Ecolor = null;//Record the color of each edge
+        private int[,] Vcolor = null;//Record the color of each vertex
+        private Graphics g = null;
+        private bool draw = false;
+
+        private int FindEdge(int v1, int v2)
+        {
+            for (int j = 0; j != n; ++j )
+                if(IM[v1, j] == 1 && IM[v2, j] == 1)
+                    return j;
+            return -1;
+        }
+
+        private void FindFan(List<int> F, int u, int v)
+        {
+            int x = -1;
+            for (int j = 0; j != n; ++j)
+            {
+                int xx = -1;
+                if (IM[u, j] == 1 && IM[v, j] == 0)//find a edge connected to u but not to v
+                    for (int i = 0; i != m; ++i)
+                        if(IM[i, j] == 1 && i != u)//find another vertex x of that edge
+                        {
+                            xx = i;
+                            break;
+                        }
+                if (xx == -1 || Ecolor[j] == 0)
+                    continue;
+                if (!F.Contains(xx) && Vcolor[v, Ecolor[j]] == 0)
+                {
+                    x = xx;
+                    break;
+                }
+            }
+            if (x == -1)//F is maximal
+                return;
+            else 
+            {
+                F.Add(x);
+                FindFan(F, u, x);
+            }
+        }
+
+        private void FindPath(List<int> path, int start, ref int end, int d, int c)
+        {
+            for (int j = 0; j != n; ++j)
+                if (IM[start, j] == 1 && Ecolor[j] == d)
+                {
+                    path.Add(j);
+                    for (int i = 0; i != m; ++i)
+                        if (IM[i, j] == 1 && i != start)
+                        {
+                            end = i;
+                            FindPath(path, i, ref end, c, d);
+                        }
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Input
+            m = Convert.ToInt32(mv.Text);
+            n = Convert.ToInt32(nv.Text);
+            IM = new int[m, n];//incidence matrix
+            string cin = input.Text;
+            int i = 0, j = 0;
+            foreach (char c in cin)
+            {
+                if (c != '1' && c != '0')
+                    continue;
+                else if (c == '1')
+                    IM[i, j] = 1;
+                else
+                    IM[i, j] = 0;
+                if (++j == n)
+                {
+                    ++i;
+                    j = 0;
+                }
+            }
+            ///////////////////////////////////////
+            //Misra & Gries edge coloring algorithm
+            Ecolor = new int[n];//Record the color of each edge
+            Vcolor = new int[m, m + 1];//Record the colors not free on each vertex
+            /*color is an integer from 1; 
+             *0 indicates the eadge hasn't been colored*/
+            for (int edge = 0; edge != n; ++edge )
+            {
+                //find u, v relative to the edge
+                int u = -1, v = 0;
+                for (i = 0; i != m; ++i)
+                    if (IM[i, edge] == 1)
+                        if(u == -1)
+                            u = i;
+                        else
+                        {
+                            v = i;
+                            break;
+                        }
+                //Find maximal fan of u
+                List<int> F = new List<int>();
+                F.Add(v);
+                FindFan(F, u, v);
+                int k = F.Count;
+                /*now we have F[0:k-1] be a maximal fan of u*/
+                int c = 0, d = 0;//two colors
+                for (int color = 1; color != m + 1; ++color)
+                    if (Vcolor[u, color] == 0)//means color is free on u
+                    {
+                        c = color;
+                        break;
+                    }
+                for (int color = 1; color != m + 1; ++color)
+                    if (Vcolor[F[k - 1], color] == 0)//means color is free on F[k-1]
+                    {
+                        d = color;
+                        break;
+                    }
+                //invert the cdu path
+                /*note if there is a path it must start from u with color d*/
+                if (c != d)
+                {
+                    List<int> path = new List<int>();
+                    int end = -1;
+                    FindPath(path, u, ref end, d, c);
+                    if (end != -1)//there is a such path, then invert it
+                    {
+                        int tempc = c + d;
+                        foreach (int p in path)
+                            Ecolor[p] = tempc - Ecolor[p];
+                        Vcolor[u, d] = 0;
+                        Vcolor[u, c] = 1;
+                        Vcolor[end, c] = 1 - Vcolor[end, c];
+                        Vcolor[end, d] = 1 - Vcolor[end, d];
+                    }
+                }
+                //find w
+                int w = 0;
+                for(int index = 0; index != k; ++index)
+                    if (Vcolor[F[index], d] == 0)
+                    {
+                        w = index;
+                        break;
+                    }
+                //Rotate F'[0:w] and set C(u,F[w])=d
+                for (int index = 0; index != w; ++index)
+                {
+                    int e1 = FindEdge(F[index], u);
+                    int e2 = FindEdge(F[index + 1], u);
+                    Vcolor[F[index], Ecolor[e1]] = 0;
+                    Vcolor[F[index], Ecolor[e2]] = 1;
+                    Ecolor[e1] = Ecolor[e2];
+                    if (index + 1 == w)
+                    {
+                        int ew = FindEdge(F[w], u);
+                        Vcolor[F[w], Ecolor[ew]] = 0;
+                        Ecolor[ew] = 0;
+                    }
+                }
+                Ecolor[FindEdge(u, F[w])] = d;
+                Vcolor[u, d] = 1;
+                Vcolor[F[w], d] = 1;
+            }
+            /////////////////////////////////
+            //Output
+            string cout = "";
+            int max = 0;
+            foreach (int c in Ecolor)
+                if (c > max)
+                    max = c;
+            cout += "Number of Colors: ";
+            cout += Convert.ToString(max) + "\r\n";
+            cout += "Color of Each Edge:\r\n";
+            for (j = 0; j != n; ++j)
+                cout += Convert.ToString(Ecolor[j]) + " ";
+            cout += "\r\n\r\n";
+            cout += "Confirmation of Correctness:\r\n";
+            for (i = 0; i != m; ++i)
+            {
+                cout += "Connected to Vertex " + Convert.ToString(i) + ":\r\n";
+                for (j = 0; j != n; ++j)
+                    if (IM[i, j] == 1)
+                        cout += "Edge " + Convert.ToString(j) + ": " + Convert.ToString(Ecolor[j]) + "\r\n";
+            }
+            output.Text = cout;
+            //////////////////////////////////
+            //If the number of colors is less than 8 then draw the graph
+            if (max > 7)
+            {
+                panel.Visible = false;
+                draw = false;
+            }
+            else
+            {
+                panel.Visible = true;
+                draw = true;
+            }
+        }
+
+        private void 导入ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.OpenFile());
+                string temp = sr.ReadLine();
+                string m = "", n = "";
+                bool flag = true;
+                foreach (char c in temp)
+                {
+                    if (c >= '0' && c <= '9')
+                        if (flag)
+                            m += c;
+                        else
+                            n += c;
+                    else
+                        if (flag)
+                            flag = false;
+                }
+                mv.Text = m;
+                nv.Text = n;
+                input.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Project:\tImplementation of Edge Coloring\r\nAuthor:\t李玉翎飞");
+        }
+
+        private void readmeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1) m is the number of vertexes and n is the number of edges.\r\n2) The incidence matrix is inputed in a format of m*n.\r\n3) A test file can be imported.\r\n4) Only 7 colors are supported for drawing.");
+        }
+    }
+}
